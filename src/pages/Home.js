1cd619dafd84c04ed5359be0435b8080d398409b
gagev <gagevank@gmail.com>
2022-03-17 23:11:57 -0400

    Adding Assignment description to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home(){
     return(
         <div>
-            Home
+            This is a very basic website using ReactJS and retrieves information from an airtable database. The database includes Students and Advisors and their respective pages list all related information.
         </div>
     );
 }
