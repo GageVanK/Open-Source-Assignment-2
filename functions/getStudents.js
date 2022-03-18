a012794fdbbdb6ae1e9d4016e235d248facf8ff5
gagev <gagevank@gmail.com>
2022-03-17 22:04:38 -0400

    getStudents function

---
+++ functions/getStudents.js
@@ -0,0 +1,22 @@
+const Airtable = require('airtable')
+
+Airtable.configure({
+    apiKey: 'key4cFwk1ynHHzTTL'
+})
+const base = require('airtable').base('appMJX941hoKvNNHg')
+const table = base('Students')
+
+exports.handler = async(event) => {
+    const records = await table
+        .select({filterByFormula:  'first != ""'})
+        .firstPage();
+
+    const formattedRecords = records.map((record) => ({
+        id: record.id,
+        fields: record.fields
+    }))
+    return {
+        statusCode: 200,
+        body: JSON.stringify(records),
+    }
+}
--- src/pages/Students.js
+++ src/pages/Students.js
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
+
 export default function Students(){
+    const [students, setStudents] = useState([]);
+
+    useEffect (() => {
+        const loadStudents = async () => {
+            try{
+
+                const res = await fetch('/.netlify/functions/getStudents')
+                const student = await res.json()
+                setStudents(student)
+            } catch (err){
+                console.error(err)
+            }
+            loadStudents();
+        }
+    }, [])
     return(
         <div>
-            Students
+            <h1>Students</h1>
+
+            <ul>
+                {students.map(student => 
+                <li>{student.fields.first}</li>)}
+            </ul>
         </div>
+        
     );
 }
