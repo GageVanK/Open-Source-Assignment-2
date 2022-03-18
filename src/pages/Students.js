53de00a13cdb9dff6772a72592a5752d89b26676
gagev <gagevank@gmail.com>
2022-03-17 22:50:38 -0400

    it works now i am listing the other fields was just testing and trying to get the first names to display

--- src/pages/Students.js
+++ src/pages/Students.js
@@ -24,7 +24,7 @@ export default function Students(){
             <ul>
                 {students.map((First) => (
                 <li>
-                {First.fields.First}
+                {First.fields.First} " " {First.fields.Last}
                 </li>
                 ))}
             </ul>
