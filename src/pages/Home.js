1d34f9d2053e412e0b932e7d5190aa6e76b0733b
gagev <gagevank@gmail.com>
2022-03-17 23:16:57 -0400

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 export default function Home(){
     return(
         <div>
-        <li>https://github.com/GageVanK/Open-Source-Assignment-2</li>
+        <a href="https://github.com/GageVanK/Open-Source-Assignment-2">Github Repo</a>
+        
             <p>This is a very basic website using ReactJS and retrieves information from an airtable database. The database includes Students and Advisors and their respective pages list all related information.</p>
         </div>
     );
