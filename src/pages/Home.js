cf086e93ac67c8acd4a1f27bab2abf4090eaf189
gagev <gagevank@gmail.com>
2022-03-17 23:14:34 -0400

    adding github link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 export default function Home(){
     return(
         <div>
-            This is a very basic website using ReactJS and retrieves information from an airtable database. The database includes Students and Advisors and their respective pages list all related information.
+        <li>https://github.com/GageVanK/Open-Source-Assignment-2</li>
+            <p>This is a very basic website using ReactJS and retrieves information from an airtable database. The database includes Students and Advisors and their respective pages list all related information.</p>
         </div>
     );
 }
